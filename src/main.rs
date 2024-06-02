25def2c30fc856596ddf3a452152a38add448f65
JersonCortes <JersonCortes@proton.me>
2024-06-01 23:00:56 -0600

    Added About me section

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use leptos::*;
+use leptos_meta::*;
 
 #[component]
 fn App() -> impl IntoView {
 
+    let (count, set_count) = create_signal(0);
+
     view! {
-    
+        //<Meta name="color-scheme" content="dark"/>  
+
     }
 }
 
 fn main() {
     mount_to_body(|| {
         view! {
-            <App/>
         }
     })
 }
