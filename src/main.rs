0e32614f287ec235cfb3e1f0073e473e2e43f63a
JersonCortes <JersonCortes@proton.me>
2024-06-03 22:55:18 -0600

    Updated devcontainer, Added new design

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use leptos_meta::*;
 #[component]
 fn App() -> impl IntoView {
 
-    let (count, set_count) = create_signal(0);
-
     view! {
         //<Meta name="color-scheme" content="dark"/>  
 
