8ac6b1893520c0f576b3a5313c5ca055b1e90b93
JersonCortes <JersonCortes@proton.me>
2024-07-02 12:47:12 -0600

    cleaned tags, implemented logic

--- src/components/home_page.rs
+++ src/components/home_page.rs
@@ -4,7 +4,7 @@ use leptos::*;
 fn About() -> impl IntoView {
     view! {
         <h2>About me</h2>
-        <hr></hr>
+        <hr/>
         <p>
             "I am a software engineer who enjoys low level programming as well as understanding complex systems with 1 year of experience as a Data Analyst using Python with a European team. I'm interested on following my passion with low level programming. Since more than two years ago I've been using Linux in my home servers as well as for multiple projects. Since more than 2 years ago I commited to using Linux as my daily operating system. Right now I'm focused in c/c++ and Linux kernel development related opportunities."
         </p>
@@ -16,7 +16,7 @@ fn About() -> impl IntoView {
 fn Projects() -> impl IntoView {
     view! {
         <h2>Projects</h2>
-        <hr></hr>
+        <hr/>
         <a href="https://github.com/JersonCortes/telescope-podman">
             <h3>Container.Telescope.nvim</h3>
         </a>
@@ -36,7 +36,7 @@ fn Projects() -> impl IntoView {
 fn Recent_blogs() -> impl IntoView {
     view! {
         <h2>Currently Learning</h2>
-        <hr></hr>
+        <hr/>
         <h3>Kernel Development</h3>
         <p>
             Working on my first kernel patch.
@@ -50,8 +50,8 @@ fn Recent_blogs() -> impl IntoView {
 #[component]
 pub fn Main() -> impl IntoView {
     view! {
-        <About></About>
-        <Projects></Projects>
-        <Recent_blogs></Recent_blogs>
+        <About/>
+        <Projects/>
+        <Recent_blogs/>
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@ use leptos::*;
 mod components{
     pub mod sidebar;
     pub mod home_page;
+    pub mod blog;
 }
 
 use components::sidebar::Sidebar;
 use components::home_page::Main;
+use components::blog::Blog;
 
 #[component]
 fn App() -> impl IntoView {
+    let (value, set_value) = create_signal(true);
+
     view! {
     	<div class="container">
-            <Sidebar></Sidebar>
+            <Sidebar/>
             <div class="text-container">
-                <Main></Main>
+                {move || if value(){
+                    view! {<Main/>}
+                } else {
+                    view! {<Blog/>}
+                }}
             </div>
     	</div>
     }
