6b6e00d7c1e7eb78cb00a09e3f38a907e091a22c
Jerson Cortes <JersonCortes@proton.me>
2024-12-06 14:46:09 -0600

    Cleaned code further, added meta description correctly

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use leptos::prelude::*;
-use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title, Meta};
+use leptos_meta::{provide_meta_context, Stylesheet, Title};
 use leptos_router::{
     components::{Route, Router, Routes},
     StaticSegment,
@@ -11,24 +11,6 @@ use crate::routes::{
 };
 
 use crate::components::side_bar::Sidebar;
-
-pub fn shell(options: LeptosOptions) -> impl IntoView {
-    view! {
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <AutoReload options=options.clone() />
-                <MetaTags/>
-                <Meta name="description" content="Software developer."/>
-            </head>
-            <body>
-                <App/>
-            </body>
-        </html>
-    }
-}
 
 #[component]
 pub fn App() -> impl IntoView {
