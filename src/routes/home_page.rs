6e244f959de35565a01c0ab92465d1c5306745a7
Jerson Cortes <JersonCortes@proton.me>
2025-03-06 06:19:12 -0600

    Gave it mopre color

--- src/routes/home_page.rs
+++ src/routes/home_page.rs
@@ -11,7 +11,7 @@ pub fn HomePage() -> impl IntoView {
 #[component]
 fn About() -> impl IntoView {
     view! {
-        <h2>Jerson Cortes P.</h2>
+        <h2><span class="highlight">J</span>erson <span class="highlight">C</span>ortes <span class="highlight">P.</span></h2>
         <p>
             "New version coming soon."
         </p>
