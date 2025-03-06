0885327a133abc4649d446d0ebc0dcf220d02ff8
Jerson Cortes <JersonCortes@proton.me>
2025-03-06 06:01:22 -0600

    Preparing for new changes

--- src/routes/home_page.rs
+++ src/routes/home_page.rs
@@ -13,7 +13,7 @@ fn About() -> impl IntoView {
     view! {
         <h2>Jerson Cortes P.</h2>
         <p>
-            "I am a software developer and data analyst with passion for open source. My expertise lies in full-stack web development with Rust, data analysis with Python, and low-level systems."
+            "New version coming soon."
         </p>
 
     }
